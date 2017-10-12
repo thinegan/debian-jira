82cdf5a3288f505d701a63698407e793923f381e
Thinegan Ratnam <thinegan@gmail.com>
2017-10-12 22:14:02 +0800

    Remove/cleanup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,19 +3,10 @@
 # check if the `server.xml` file has been changed since the creation of this
 # Docker image. If the file has been changed the entrypoint script will not
 # perform modifications to the configuration file.
-if [ "$(stat --format "%Y" "${JIRA_INSTALL}/conf/server.xml")" -eq "0" ]; then
-    echo "${JIRA_INSTALL}/conf/server.xml has not previously been modified with custom proxy settings"
-
-    # Backup conf/server.xml
-    cp "${JIRA_INSTALL}/conf/server.xml" "${JIRA_INSTALL}/conf/server.xml.backup"
-
     if [ -n "${JIRASERVERNAME}" ]; then
         echo "Updating '$JIRASERVERNAME' as connector proxyName"
         sed -i 's/connectionTimeout="20000"/connectionTimeout="20000" scheme="https" proxyName="'${JIRASERVERNAME}'" proxyPort="443"/g' ${JIRA_INSTALL}/conf/server.xml
     fi
-else
-    echo "Skipping modification of ${JIRA_INSTALL}/conf/server.xml as it was already modified after initial Docker image creation"
-fi
 
 # Run in foreground
 exec "$@"
